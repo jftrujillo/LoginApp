91f4a84da06ece76cd806730dd7b6c14f1c4356a
Jhon <jftrujillo@unicauca.edu.co>
2016-10-05 07:36:40 -0500

    thrid commit

--- app/src/main/java/com/example/jhon/loginapp/MainActivity.java
+++ app/src/main/java/com/example/jhon/loginapp/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.jhon.loginapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText nombre,contrasena;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nombre = (EditText) findViewById(R.id.nombre_usuario);
+        contrasena = (EditText) findViewById(R.id.contrasena);
+        btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
---
+++ app/src/main/java/com/example/jhon/loginapp/util/ValidateStrings.java
@@ -0,0 +1,8 @@
+package com.example.jhon.loginapp.util;
+
+/**
+ * Created by jhon on 5/10/16.
+ */
+
+public class ValidateStrings {
+}
