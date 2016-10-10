accb8038ae9467a589031980d2e3cb3edb45655e
Jhon <jftrujillo@unicauca.edu.co>
2016-10-10 08:07:47 -0500

    Agrega listeners de elementos de layout

--- app/src/main/java/com/example/jhon/loginapp/DashBoardActivity.java
+++ app/src/main/java/com/example/jhon/loginapp/DashBoardActivity.java
@@ -3,14 +3,19 @@ package com.example.jhon.loginapp;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SwitchCompat;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.jhon.loginapp.util.Constants;
 
-public class DashBoardActivity extends AppCompatActivity {
+public class DashBoardActivity extends AppCompatActivity implements View.OnClickListener {
     TextView hola;
     SharedPreferences sharedPreferences;
     String nombreUsuario;
+    SwitchCompat dulces,asados,comidaRapida;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +30,32 @@ public class DashBoardActivity extends AppCompatActivity {
                 Constants.userName,"NO USER NAME");
 
         hola.setText("Hola: " + nombreUsuario);
+        dulces = (SwitchCompat) findViewById(R.id.dulces);
+        dulces.setOnClickListener(this);
+        asados = (SwitchCompat) findViewById(R.id.asados);
+        asados.setOnClickListener(this);
+        comidaRapida = (SwitchCompat) findViewById(R.id.comida_rapida);
+        comidaRapida.setOnClickListener(this);
+        btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(this);
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.dulces:
+
+                break;
+            case R.id.asados:
+
+                break;
+            case R.id.comida_rapida:
+
+                break;
+            case R.id.btn:
+
+                break;
+        }
     }
 }
